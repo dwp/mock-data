f5771eefb3aef8b7ea3853144c78e51514c5dcc7
Simone Duca <simone.duca@gmail.com>
2018-05-09 15:53:18 +0100

    Better var names. Update README.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # mock-data
-Generate random data.
+It generates a `mock-data.json` file with 100 objects.
+
+Object example:
+```
+{
+  "lastName": "Swift",
+  "firstName": "Carmine",
+  "nino": "6925",
+  "address": {
+    "street": "7022 Bryon Shoals",
+    "city": "Tylerview",
+    "county": "Avon"
+  },
+  "phone": "256-764-0829"
+}
+```
+
+## Usage
+
+__Install the dependencies:__
+
+`npm install`
+
+__Create file:__
+
+```npm start```
+
+## Credits
+
+Built with [Faker.js](https://github.com/marak/Faker.js/).
--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const express = require('express');
 
 const app = express();
 
-const users = [];
+const data = [];
 
 for (i = 0; i <= 100; i++) {
-  var data = {};
-  data.lastName = faker.fake("{{name.lastName}}");
-  data.firstName = faker.fake("{{name.firstName}}");
-  data.nino = faker.fake("{{random.number}}");
-  data.address = {
+  let person = {};
+  person.lastName = faker.fake("{{name.lastName}}");
+  person.firstName = faker.fake("{{name.firstName}}");
+  person.nino = faker.fake("{{random.number}}");
+  person.address = {
     'street': faker.fake("{{address.streetAddress}}"),
     'city': faker.fake("{{address.city}}"),
     'county': faker.fake("{{address.county}}"),
   }
-  data.phone = faker.fake("{{phone.phoneNumberFormat}}");
-  users.push(data);
+  person.phone = faker.fake("{{phone.phoneNumberFormat}}");
+  data.push(person);
 }
 
-fs.writeFile('mock-data.json', JSON.stringify(users), (err) => {
+fs.writeFile('mock-data.json', JSON.stringify(data), (err) => {
   if (err) throw err;
   console.log('The file has been saved!');
 });
